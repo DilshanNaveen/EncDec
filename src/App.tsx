1912763b6829915cb08a22dadb05062f01d1221c
Dilshan Naveen <dilshan@vertex.lk>
2023-04-10 01:26:51 +0530

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React, { useState } from 'react';
-import { Box, Button, Container, Tab, Tabs, TextField, Typography, Snackbar, Alert as MuiAlert, IconButton, InputAdornment } from '@mui/material';
-import styled from '@emotion/styled';
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  Tab,
+  Tabs,
+  TextField,
+  Typography,
+  Snackbar,
+  Alert as MuiAlert,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
+import styled from "@emotion/styled";
 import { AES, enc } from "crypto-js";
-import FileCopyIcon from '@mui/icons-material/FileCopy';
+import FileCopyIcon from "@mui/icons-material/FileCopy";
 
 const GradientBackground = styled(Container)`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
+`;
+
+const GradientButton = styled(Button)`
+  background: linear-gradient(135deg, #74ebd5 10%, #9face6 90%);
+  border-radius: 3;
+  border: 0;
+  color: white;
+  height: 55px;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(59, 169, 238, .3);
 `;
 
 interface TabPanelProps {
@@ -29,23 +51,27 @@ function TabPanel(props: TabPanelProps) {
 
 function App() {
   const [tabValue, setTabValue] = useState(0);
-  const [plainText, setPlainText] = useState('');
-  const [encryptKey, setEncryptKey] = useState('');
-  const [encryptedText, setEncryptedText] = useState('');
-  const [decryptKey, setDecryptKey] = useState('');
-  const [decryptedText, setDecryptedText] = useState('');
-  const [alert, setAlert] = useState<any>({ open: false, message: '', severity: 'error' });
+  const [plainText, setPlainText] = useState("");
+  const [encryptKey, setEncryptKey] = useState("");
+  const [encryptedText, setEncryptedText] = useState("");
+  const [decryptKey, setDecryptKey] = useState("");
+  const [decryptedText, setDecryptedText] = useState("");
+  const [alert, setAlert] = useState<any>({
+    open: false,
+    message: "",
+    severity: "error",
+  });
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-    setPlainText('');
-    setEncryptKey('');
-    setEncryptedText('');
-    setDecryptKey('');
-    setDecryptedText('');
+    setPlainText("");
+    setEncryptKey("");
+    setEncryptedText("");
+    setDecryptKey("");
+    setDecryptedText("");
     setTabValue(newValue);
   };
 
-  const showAlert = (message: string, severity: 'error' | 'success') => {
+  const showAlert = (message: string, severity: "error" | "success") => {
     setAlert({ open: true, message, severity });
   };
 
@@ -56,11 +82,11 @@ function App() {
   const handleEncrypt = () => {
     // Encryption logic here
     if (!plainText) {
-      showAlert('Please enter plain text to encrypt.', 'error');
+      showAlert("Please enter plain text to encrypt.", "error");
       return;
     }
     if (!encryptKey) {
-      showAlert('Please enter a key for encryption.', 'error');
+      showAlert("Please enter a key for encryption.", "error");
       return;
     }
 
@@ -71,11 +97,11 @@ function App() {
   const handleDecrypt = () => {
     // Decryption logic here
     if (!encryptedText) {
-      showAlert('Please enter encrypted text to decrypt.', 'error');
+      showAlert("Please enter encrypted text to decrypt.", "error");
       return;
     }
     if (!decryptKey) {
-      showAlert('Please enter a key for decryption.', 'error');
+      showAlert("Please enter a key for decryption.", "error");
       return;
     }
 
@@ -89,14 +115,14 @@ function App() {
       await navigator.clipboard.writeText(text);
       setAlert({
         open: true,
-        message: 'Text copied to clipboard!',
-        severity: 'success',
+        message: "Text copied to clipboard!",
+        severity: "success",
       });
     } catch (err) {
       setAlert({
         open: true,
-        message: 'Failed to copy the text!',
-        severity: 'error',
+        message: "Failed to copy the text!",
+        severity: "error",
       });
     }
   };
@@ -107,7 +133,10 @@ function App() {
         Encryption & Decryption
       </Typography>
       <Typography variant="body1" align="center" gutterBottom>
-        This web app allows you to securely encrypt and decrypt text using the AES encryption algorithm. Enter the plain text and a secret key to encrypt the text. To decrypt the encrypted text, provide the same secret key used for encryption.
+        This web app allows you to securely encrypt and decrypt text using the
+        AES encryption algorithm. Enter the plain text and a secret key to
+        encrypt the text. To decrypt the encrypted text, provide the same secret
+        key used for encryption.
       </Typography>
       <br />
       <br />
@@ -121,11 +150,29 @@ function App() {
           <Typography variant="h6">Plain Text</Typography>
         </Box>
         <Box mt={1}>
-          <TextField multiline label="Plain Text" rows={4} fullWidth value={plainText} onChange={(e) => setPlainText(e.target.value)} />
+          <TextField
+            multiline
+            label="Plain Text"
+            rows={4}
+            fullWidth
+            value={plainText}
+            onChange={(e) => setPlainText(e.target.value)}
+          />
         </Box>
         <Box mt={2}>
-          <TextField type='password' label="Key" value={encryptKey} onChange={(e) => setEncryptKey(e.target.value)} />
-          <Button onClick={handleEncrypt}>Encrypt</Button>
+          <TextField
+            type="password"
+            label="Key"
+            value={encryptKey}
+            onChange={(e) => setEncryptKey(e.target.value)}
+          />
+          <GradientButton
+            variant="contained"
+            onClick={handleEncrypt}
+            sx={{ ml: 2 }}
+          >
+            Encrypt
+          </GradientButton>
         </Box>
         <Box mt={1}>
           <Typography variant="h6">Encrypted Text</Typography>
@@ -144,11 +191,10 @@ function App() {
                     <FileCopyIcon />
                   </IconButton>
                 </InputAdornment>
-              )
+              ),
             }}
           />
         </Box>
-
       </TabPanel>
 
       <TabPanel value={tabValue} index={1}>
@@ -156,11 +202,29 @@ function App() {
           <Typography variant="h6">Encrypted Text</Typography>
         </Box>
         <Box mt={1}>
-          <TextField multiline rows={4} label="Encrypted Text" fullWidth value={encryptedText} onChange={(e) => setEncryptedText(e.target.value)} />
+          <TextField
+            multiline
+            rows={4}
+            label="Encrypted Text"
+            fullWidth
+            value={encryptedText}
+            onChange={(e) => setEncryptedText(e.target.value)}
+          />
         </Box>
         <Box mt={2}>
-          <TextField type='password' label="Key" value={decryptKey} onChange={(e) => setDecryptKey(e.target.value)} />
-          <Button onClick={handleDecrypt}>Decrypt</Button>
+          <TextField
+            type="password"
+            label="Key"
+            value={decryptKey}
+            onChange={(e) => setDecryptKey(e.target.value)}
+          />
+          <GradientButton
+            onClick={handleDecrypt}
+            variant="contained"
+            sx={{ ml: 2 }}
+          >
+            Decrypt
+          </GradientButton>
         </Box>
         <Box mt={1}>
           <Typography variant="h6">Decrypted Text</Typography>
@@ -184,8 +248,18 @@ function App() {
           />
         </Box>
       </TabPanel>
-      <Snackbar open={alert.open} autoHideDuration={6000} onClose={handleCloseAlert} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
-        <MuiAlert onClose={handleCloseAlert} severity={alert.severity} elevation={6} variant="filled">
+      <Snackbar
+        open={alert.open}
+        autoHideDuration={6000}
+        onClose={handleCloseAlert}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <MuiAlert
+          onClose={handleCloseAlert}
+          severity={alert.severity}
+          elevation={6}
+          variant="filled"
+        >
           {alert.message}
         </MuiAlert>
       </Snackbar>
@@ -194,4 +268,3 @@ function App() {
 }
 
 export default App;
-
